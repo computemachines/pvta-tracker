dabef3a07472ecbe7eb938fc000d1ced84cdd703
Tyler Parker <tparker@student.umass.edu>
2013-02-27 10:27:31 -0500

    Interface with the ActiveRecord DB for rails, expose models created for them.

---
+++ DataStore.rb
@@ -0,0 +1,30 @@
+
+require 'active_record'
+require_relative '../webapp/app/models/route.rb'
+require_relative '../webapp/app/models/bus.rb'
+require_relative '../webapp/app/models/history.rb'
+require_relative '../webapp/app/models/scheduled_stop.rb'
+
+require_relative 'getData'
+
+class DataStore
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => 'sqlite3',
+    :database => '../webapp/db/development.sqlite3'
+  )
+
+  def Route
+    Route
+  end
+  def Bus
+    Bus
+  end
+  def Stop
+    Stop
+  end
+  def ScheduledStop
+    ScheduledStop
+  end
+end
+
